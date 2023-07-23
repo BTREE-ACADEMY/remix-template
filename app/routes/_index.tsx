455de7a1e033f3a0b27ce95c46d4ace539d3f04e
daniel kim <daniel.c.kim@live.com>
2023-07-23 12:27:34 -0700

    Revert "make initial version simpler"
    This reverts commit 06df5a953642adb107229cd67c425fd0c67443f1.

---
+++ app/routes/_index.tsx
@@ -0,0 +1,11 @@
+export default function Index() {
+  return (
+    <div>
+      <img
+        src="https://quiz.btreecode.com/images/BTREE_logo_re.svg"
+        style={{ height: 100 }}
+      />
+      <h1 className="text-3xl font-bold text-sky-600">Hello Btree</h1>
+    </div>
+  );
+}
--- docs/temp.md
+++
@@ -1,6 +0,0 @@
-```jsx
-import { LinksFunction } from "@remix-run/node";
-import styles from "./tailwind.css";
-
-export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
-```
