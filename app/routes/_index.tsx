08372e0afcff654838c0d91546f4dd7f86799f6f
daniel kim <daniel.c.kim@live.com>
2023-07-23 22:21:14 -0700

    daisyui added

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,7 @@
 export default function Index() {
   return (
     <div>
-      <img
-        src="https://quiz.btreecode.com/images/BTREE_logo_re.svg"
-        style={{ height: 100 }}
-      />
-      <h1 className="text-3xl font-bold text-sky-600">Hello Btree</h1>
+      <h1 className="text-3xl font-bold underline">hello btree</h1>
     </div>
   );
 }
